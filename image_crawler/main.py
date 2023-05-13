4958a064faf201c4d771e4e158d3a8b0e56ffc4a
Nergis <127741113+Nergis-Dev@users.noreply.github.com>
2023-05-14 00:55:57 +0900

    Add files via upload

---
+++ image_crawler/main.py
@@ -0,0 +1,20 @@
+import requests, uuid
+
+with open("ww.txt", "r", encoding="utf-8") as file:
+    url_list = file.readlines()
+
+uu = []
+
+for u in url_list:
+    u = u.replace("\n", "")
+    uu.append(f"{u}")
+
+print("uu Done!")
+
+for i in uu:
+    # 이미지 다운로드
+    response = requests.get(i)
+    # 이미지를 파일에 저장
+    with open(f'./art/{str(uuid.uuid4())}.png', 'wb') as f:
+        f.write(response.content)
+    print(f"Save: ./art/{str(uuid.uuid4())}.png !")
