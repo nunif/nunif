36a5d7a87da36b4de52ed51bfe0d9518babdbf14
Nergis <127741113+Nergis-Dev@users.noreply.github.com>
2023-05-14 01:11:23 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 nvidia-smi
 sudo apt install git-core libmagickwand-dev libsnappy-dev libraqm-dev python-is-python3 python3 python3-pip python3-dev python3-tk -y
-git clone https://github.com/nagadomi/nunif.git
 cd nunif
 pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
 pip3 install -r requirements.txt
