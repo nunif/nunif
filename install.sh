687280d7162473ebbf95a8a402545bd133c1f7cc
Nergis <127741113+Nergis-Dev@users.noreply.github.com>
2023-05-14 13:38:06 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 nvidia-smi
 sudo apt install git-core libmagickwand-dev libsnappy-dev libraqm-dev python-is-python3 python3 python3-pip python3-dev python3-tk -y
-cd nunif && pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121 && pip3 install -r requirements.txt && mkdir data && mkdir data/dataset && mkdir data/dataset/train && mkdir data/waifu2x && mkdir data/dataset/waifu2x && mkdir models && mkdir models/waifu2x_mymodel
+cd nunif && pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121 && pip3 install -r nunif/requirements.txt && mkdir data && mkdir data/dataset && mkdir data/dataset/train && mkdir data/waifu2x && mkdir data/dataset/waifu2x && mkdir models && mkdir models/waifu2x_mymodel
